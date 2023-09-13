c379920cd0a7fb77cf9376faa67386fcd2dcc2cb
khaegenm1 <khagenbaro@live.com>
2023-09-13 10:16:31 +0530

    first commit

---
+++ src/main/java/modal/Room.java
@@ -0,0 +1,24 @@
+package modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+    private int roomNumber;
+    private double price;
+    private enum RoomType{
+        SINGLE,
+        DOUBLE,
+        DELUXE
+    };
+
+
+
+}
